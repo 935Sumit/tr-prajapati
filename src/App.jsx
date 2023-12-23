8b78bdbfabdc728f6acd85b0248b85d571fb2a89
Yogesh Prajapati <201701259@daiict.ac.in>
2023-12-23 12:07:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/header/header'
+
+
+ const App = () => {
+  return (
+    <>
+    <Header/>
+    <h1>in app file</h1>
+    </>
+  )
+}
+export default App
